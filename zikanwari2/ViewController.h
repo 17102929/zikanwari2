c7d37ceec458a46564a9739276d39e4d1f266bc5
Sora <sora@Sora-no-MacBook-Air.local>
2014-08-22 23:23:59 +0900

    Timetables
    .

--- zikanwari2/ViewController.h
+++ zikanwari2/ViewController.h
@@ -7,7 +7,35 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 
-@interface ViewController : UIViewController
+const static int WIDTH_BLOCK_NUM = 7;
+const static int HEIGHT_BLOCK_NUM = 8;
+
+
+const static int BOTTOM_HEIGHT = 100;
+
+const static int TEXTFIELD_WIDTH = 100;
+const static int TEXTFIELD_HEIGHT = 30;
+
+//#define COLOR_PURPLE [UIColor colorWithRed:0.75f green:0.51f blue:1.0f alpha:1.0f]
+//#define COLOR_SKYBLUE [UIColor colorWithRed:0.61 green:1.0f blue:1.0f alpha:1.0f]
+//#define COLOR_PINK [UIColor colorWithRed:1.0 green:0.63f blue:0.81f alpha:1.0f]
+//#define COLOR_GREEN [UIColor colorWithRed:0.81f green:1.0f blue:0.81f alpha:1.0f]
+//#define COLOR_YELLOW [UIColor colorWithRed:1.0f green:1.0f blue:0.61f alpha:1.0f]
+
+
+
+
+@interface ViewController : UIViewController<UITextFieldDelegate>{
+    UIButton *label[WIDTH_BLOCK_NUM][HEIGHT_BLOCK_NUM], *save;
+    UITextField *subject,*roomNumber,*teacher;
+    UIScrollView *scr;
+    NSArray *itemsofsegment;
+    UIDatePicker *datepicker;
+    
+}
+
+//- (IBAction)showlog;
 
 @end
--- zikanwari2/ViewController.m
+++ zikanwari2/ViewController.m
@@ -12,18 +12,262 @@
 
 @end
 
+static NSString *const dayoftheweek[WIDTH_BLOCK_NUM] = {@"", @"月", @"火", @"水", @"木", @"金", @"土",};
+
 @implementation ViewController
 
 - (void)viewDidLoad
 {
+    
+    
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    datepicker = [[UIDatePicker alloc]initWithFrame:CGRectMake(0 , 408, 300, 30)];
+    [scr addSubview:datepicker];
+    datepicker.hidden =YES;
+    
+    
+    
+    scr = [[UIScrollView alloc]init];
+    [self.view addSubview:scr];
+    
+    //画面サイズの取得
+    CGSize whole = [[UIScreen mainScreen]bounds].size;
+    
+    //一つのラベルのX座標の変数
+    float labelwidth = whole.width/WIDTH_BLOCK_NUM;
+    
+    
+    //一つのラベルのY座標の変数
+    float labelheight = (whole.height-BOTTOM_HEIGHT)/HEIGHT_BLOCK_NUM;
+    
+    
+    for(int i = 0; i <HEIGHT_BLOCK_NUM; i++){
+        for(int j = 0; j<WIDTH_BLOCK_NUM; j++){
+            label[j][i] = [[UIButton alloc]initWithFrame:CGRectMake(labelwidth*j, labelheight*i, labelwidth, labelheight )];
+            [label[j][i] setTitle:@"" forState:UIControlStateNormal];
+            label[j][i].backgroundColor =[UIColor colorWithRed:0.61 green:1.0f blue:1.0f alpha:1.0f];
+            [scr addSubview:label[j][i]];
+            
+            if(! (j == 0 || i == 0) ){
+                
+                [label[j][i] addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchDown];
+            }
+            
+        
+            
+            if(!(j == 0 && i == 0 )){
+             
+                datepicker.hidden = NO;
+                
+            }
+            
+            
+        
+            label[j][i].layer.borderColor = [UIColor blueColor].CGColor;
+            label[j][i].layer.borderWidth = 1.0f;
+            label[j][i].layer.cornerRadius = 0.0f;
+        
+        }
+    }
+    
+    
+    
+    for(int i = 0; i <WIDTH_BLOCK_NUM; i++){
+        label[i][0].backgroundColor = [UIColor colorWithRed:1.0f green:0.63f blue:0.81f alpha:1.0f];
+    }
+    
+    
+    for(int i =0; i <HEIGHT_BLOCK_NUM; i++){
+        //iをY座標としてiを増やすのをループさせることで縦全てを黄色に
+        label[0][i].backgroundColor = [UIColor colorWithRed:0.75f green:0.51f blue:1.0f alpha:1.0f];
+    }
+    
+    for(int i = 0; i <HEIGHT_BLOCK_NUM; i++){
+        [label[0][i] setTitle:[NSString stringWithFormat:@"%d" , i] forState:UIControlStateNormal];
+        
+
+    }
+    
+    //iはWIDTH_BLOCK_NUM-1になる、最終的に。dayoftheweekの配列の最大数(WIDTH_BLOCK_NUM-1)まで0から1づつ足していく。
+    for(int i = 0; i<WIDTH_BLOCK_NUM; i++){
+        [label[i][0] setTitle:dayoftheweek[i] forState:UIControlStateNormal];
+    }
+    
+    
+    subject = [[UITextField alloc]initWithFrame:CGRectMake(164-TEXTFIELD_WIDTH/2,
+                                                           484,
+                                                           TEXTFIELD_WIDTH,
+                                                           TEXTFIELD_HEIGHT)];
+    
+    roomNumber = [[UITextField alloc]initWithFrame:CGRectMake(74-TEXTFIELD_WIDTH/2,
+                                                              534,
+                                                              TEXTFIELD_WIDTH,
+                                                              TEXTFIELD_HEIGHT)];
+    
+    teacher = [[UITextField alloc]initWithFrame:CGRectMake(252-TEXTFIELD_WIDTH/2,
+                                                           534,
+                                                           TEXTFIELD_WIDTH,
+                                                           TEXTFIELD_HEIGHT)];
+        
+
+    scr.frame = self.view.bounds;
+    
+    
+    [scr addSubview:subject];
+    [scr addSubview:roomNumber];
+    [scr addSubview:teacher];
+    
+    subject.borderStyle = UITextBorderStyleRoundedRect;
+    subject.returnKeyType = UIReturnKeyNext;
+    subject.placeholder = @"subject";
+    subject.delegate = self;
+    subject.textAlignment = NSTextAlignmentCenter;
+    subject.adjustsFontSizeToFitWidth = YES;
+    
+   
+    
+    
+    
+    roomNumber.borderStyle = UITextBorderStyleRoundedRect;
+    roomNumber.returnKeyType = UIReturnKeyNext;
+    roomNumber.placeholder = @"room";
+    roomNumber.delegate = self;
+    roomNumber.textAlignment = NSTextAlignmentCenter;
+    roomNumber.adjustsFontSizeToFitWidth = YES;
+    
+    
+    
+    teacher.borderStyle = UITextBorderStyleRoundedRect;
+    teacher.returnKeyType = UIReturnKeyDone;
+    teacher.placeholder = @"teacher";
+    teacher.delegate = self;
+    teacher.textAlignment = NSTextAlignmentCenter;
+    teacher.adjustsFontSizeToFitWidth = YES;
+    
+    
+    [self registerForKeyboardNotifications];
+    
+    scr.backgroundColor = [UIColor colorWithRed:0.81f green:1.0f blue:0.81f alpha:1.0f];
+    
+    
+    /*itemsofsegment = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", @"6", @"7", nil];
+    segment = [[UISegmentedControl alloc]initWithItems:itemsofsegment];
+    segment.frame = CGRectMake(15, 500, 300, 30);
+    [scr addSubview:segment];
+    //segment.hidden = YES;
+    segment.backgroundColor = [UIColor greenColor];
+    tosegment = [[UIButton alloc]initWithFrame:CGRectMake(10, 475, 45, 45)];
+    [scr addSubview:tosegment];
+    [tosegment addTarget:self action:@selector(pushedtosegment:)forControlEvents:UIControlEventTouchUpInside];
+    tosegment.backgroundColor = [UIColor colorWithRed:1.0f green:0.66f blue:1.0f alpha:1.0];*/
+    
+    
+    
+    
 }
+
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
+    [super didReceiveMemoryWarning]; 
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+    
+    
+}
+
+
+
+
+
+
+
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    
+    
+    CGPoint scrollPoint = CGPointMake(0.0,250.0);
+    [scr setContentOffset:scrollPoint animated:YES];
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    [scr setContentOffset:CGPointZero animated:YES];
+    
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    
+    if ([textField.text isEqualToString:@""]) {
+        
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"内容を入力してください"
+                              message:@""
+                              delegate:self
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles: nil];
+        
+        // アラートビューを表示
+        [alert show];
+        
+    }
+    else{
+        if(subject == textField){
+            [roomNumber becomeFirstResponder];
+        }
+        // [self.view endEditing:YES];
+        
+        else if (roomNumber == textField){
+            [teacher becomeFirstResponder];
+        }
+        
+        else if (teacher == textField){
+            [self.view endEditing:YES];
+        }
+    }
+
+        
+    
+    return YES;
+}
+
+
+-(void)buttonAction:(UIButton*)button{
+    [button setTitle:subject.text forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    button.titleLabel.numberOfLines = 2;
+    button.titleLabel.font = [UIFont systemFontOfSize:12];
+    button.titleEdgeInsets = UIEdgeInsetsMake(HEIGHT_BLOCK_NUM, WIDTH_BLOCK_NUM-5 , 0,0);
+    
+}
+
+
+
+
+
+
+
+/*-(void)showlog
+{
+    for(int i = 0; i <HEIGHT_BLOCK_NUM; i++){
+        for(int j = 0; j<WIDTH_BLOCK_NUM; j++){
+            NSLog(@"[%d][%d] %@", i, j,label[i][j]);
+        }
+    }
+}*/
+
 
 @end
